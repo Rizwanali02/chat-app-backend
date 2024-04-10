34ffd978c0e10e4e918bbbc8dc3c97e95f21c4bd
rizwanali02 <rizwanali02hindoli@gmail.com>
2024-04-10 13:16:35 +0530

    url fixing

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,8 +8,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
-        methods: ["GET, POST"]
+        origin: ["*", "https://real-time-chat-app-six.vercel.app"],
+        methods: ["GET", "POST"] 
     }
 });
 
